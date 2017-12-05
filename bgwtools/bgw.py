d3c8b0497f8eeb975d8747b5db9010095e2eae80
Nelson Yeung <nelsyeung@icloud.com>
2017-12-05 17:42:40 +0000

    Revert "Make inteqp a separate step"
    This reverts commit 75c53292553d4f8d091409a0eac9098787383cbe.

--- bgwtools/bgw.py
+++ bgwtools/bgw.py
@@ -30,28 +30,28 @@ def create_link_files(config, dirname='.'):
                 '\n'
                 'ln -sf $QE/2-wfn/WFN 1-epsilon/WFN\n'
                 'ln -sf $QE/3-wfnq/WFN 1-epsilon/WFNq\n'
-                'ln -sf $QE/4-wfn_co/RHO 2-sigma/RHO\n'
-                'ln -sf $QE/4-wfn_co/WFN 2-sigma/WFN_inner\n'
+                'ln -sf $QE/4-wfn_co/RHO 2.1-sigma/RHO\n'
+                'ln -sf $QE/4-wfn_co/WFN 2.1-sigma/WFN_inner\n'
+                'ln -sf $QE/4-wfn_co/WFN 2.2-inteqp/WFN_co\n'
                 'ln -sf $QE/4-wfn_co/WFN 3-kernel/WFN_co\n'
                 'ln -sf $QE/4-wfn_co/WFN 4-absorption/WFN_co\n'
-                'ln -sf $QE/4-wfn_co/WFN 5-inteqp/WFN_co\n'
-                'ln -sf $QE/4-wfn_co/vxc.dat 2-sigma/vxc.dat\n'
+                'ln -sf $QE/4-wfn_co/vxc.dat 2.1-sigma/vxc.dat\n'
                 'ln -sf $QE/5-wfn_fi/WFN 4-absorption/WFN_fi\n'
-                'ln -sf $QE/5-wfn_fi/WFN 5-inteqp/WFN_fi\n'
+                'ln -sf $QE/5-wfn_fi/WFN 2.2-inteqp/WFN_fi\n'
                 'ln -sf $QE/6-wfnq_fi/WFN 4-absorption/WFNq_fi\n'
-                'ln -sf $QE/6-wfnq_fi/WFN 5-inteqp/WFNq_fi\n'
+                'ln -sf $QE/6-wfnq_fi/WFN 2.2-inteqp/WFNq_fi\n'
                 '\n'
-                'ln -sf ../1-epsilon/eps0mat 2-sigma\n'
+                'ln -sf ../1-epsilon/eps0mat 2.1-sigma\n'
                 'ln -sf ../1-epsilon/eps0mat 3-kernel\n'
                 'ln -sf ../1-epsilon/eps0mat 4-absorption\n'
-                'ln -sf ../1-epsilon/epsmat 2-sigma\n'
+                'ln -sf ../1-epsilon/epsmat 2.1-sigma\n'
                 'ln -sf ../1-epsilon/epsmat 3-kernel\n'
                 'ln -sf ../1-epsilon/epsmat 4-absorption\n'
-                'ln -sf ../2-sigma/eqp1.dat 4-absorption\n'
-                'ln -sf ../2-sigma/eqp1.dat 5-inteqp/eqp_co.dat\n'
+                'ln -sf ../2.1-sigma/eqp1.dat ./2.2-inteqp/eqp_co.dat\n'
+                'ln -sf ../2.1-sigma/eqp1.dat 4-absorption\n'
+                'ln -sf ../2.2-inteqp/eqp.dat 4-absorption/eqp_co.dat\n'
                 'ln -sf ../3-kernel/bsedmat 4-absorption\n'
                 'ln -sf ../3-kernel/bsexmat 4-absorption\n'
-                'ln -sf ../5-inteqp/eqp.dat 4-absorption/eqp_co.dat\n'
                 )
 
     helpers.make_executable(file)
@@ -81,12 +81,13 @@ def create_epsilon(config, dirname='.'):
 
 
 def create_sigma(config, dirname='.'):
-    """Create 2-sigma directory and its input files."""
-    dirpath = os.path.join(dirname, '2-sigma')
+    """Create 2.1-sigma directory and its input files."""
+    dirpath = os.path.join(dirname, '2.1-sigma')
     inp = os.path.join(dirpath, 'sigma.inp')
     clean = os.path.join(dirpath, 'clean')
     override = {
         'sigma': {
+            'number_bands': str(int(config['&system']['nbnd']) - 1),
             'band_index_min': config['pp_in']['vxc_diag_nmin'],
             'band_index_max': config['pp_in']['vxc_diag_nmax'],
         },
@@ -109,8 +110,8 @@ def create_sigma(config, dirname='.'):
 
 
 def create_inteqp(config, dirname='.'):
-    """Create 5-inteqp directory and its input files."""
-    dirpath = os.path.join(dirname, '5-inteqp')
+    """Create 2.2-inteqp directory and its input files."""
+    dirpath = os.path.join(dirname, '2.2-inteqp')
     inp = os.path.join(dirpath, 'inteqp.inp')
     clean = os.path.join(dirpath, 'clean')
 
